8a22c31a1fc55289e9e0f7211e06b08a6167ea51
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-03-22 17:16:11 +0530

    Update Insertion Sort.c

--- C/Insertion Sort.c
+++ C/Insertion Sort.c
@@ -3,35 +3,35 @@
 #include <stdlib.h>
 #define MAX 100
 
-void sort(int stack[], int length) {
+void sort(int set[], int length) {
 	for (int i = 1; i < length; i++) {
-		int value = stack[i];
-		while (stack[i - 1] > value) {
-			stack[i] = stack[i - 1];
+		int value = set[i];
+		while (set[i - 1] > value) {
+			set[i] = set[i - 1];
 			i--;
 		}
-		stack[i] = value;
+		set[i] = value;
 	}
 }
 
-void dump(int arr[], int length) {
+void dump(int set[], int length) {
 	for (int i = 0; i < length; i++)
-		printf("\t%s%d|", arr[i] <= 9 ? " " : "", arr[i]);
+		printf("\t%s%d|", set[i] <= 9 ? " " : "", set[i]);
 	printf("\n");
 }
 
 int main() {
-	int stack[MAX];
+	int set[MAX];
 	printf("Enter number of elements (MAX %d): ", MAX);
 	int n;
 	scanf("%d", &n);
 	srand(time(NULL));
-	for (int i = 0; i < n; i++) stack[i] = rand() % 50;
+	for (int i = 0; i < n; i++) set[i] = rand() % 50;
 	printf("Unsorted list:\t");
-	dump(stack, n);
+	dump(set, n);
 	printf("\t\t...sorting...");
-	sort(stack, n);
+	sort(set, n);
 	printf("\rSorted list:\t");
-	dump(stack, n);
+	dump(set, n);
 	return 0;
 }
