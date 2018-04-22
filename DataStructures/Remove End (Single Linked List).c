1b51dd9b20068ff8d5beac0b4c04cb11e0e4e9e4
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-04-22 14:56:22 +0530

    Create Remove End (Single Linked List).c

---
+++ DataStructures/Remove End (Single Linked List).c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+	int data; struct node *next;
+}*head;
+void traverse() {
+	struct node* temp = head;
+	while (temp->next != NULL) {
+		printf("%d  =>  ", temp->data);
+		temp = temp->next;
+	}
+	printf("%d\n", temp->data);
+}
+void add(int value) {
+	struct node* node =
+		(struct node*)malloc(sizeof(struct node));
+	node->data = value;
+	if (head == NULL) node->next = NULL;
+	else node->next = head;
+	head = node;
+}
+void deleteEnd() {
+	struct node *cpt;
+	if (head == NULL) {
+		printf("ERROR: Items not found.\n");
+		return;
+	}
+	struct node *ptr = head;
+	while (ptr->next != NULL) {
+		cpt = ptr;
+		ptr = ptr->next;
+	}
+	cpt->next = NULL;
+}
+
+
+int main() {
+	printf("Options:\n1. Add\n2. Traverse\n3. Remove from end\n0. Exit\n");
+	int choice = -1;
+	while (choice != 0) {
+		printf("Your choice: ");
+		scanf("%d", &choice);
+		int value = -1;
+		if (choice == 1) {
+			printf("Enter number to add: ");
+			scanf("%d", &value);
+			add(value);
+		} else if (choice == 2) traverse();
+		else if (choice == 3) deleteEnd();
+	}
+	return 0;
+}
