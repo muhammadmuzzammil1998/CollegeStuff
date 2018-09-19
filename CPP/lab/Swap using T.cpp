847918491b8d8880ffa0df6b5ba8400bd2c139c7
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-09-20 00:33:11 +0530

    Update Swap using T.cpp

--- CPP/lab/Swap using T.cpp
+++ CPP/lab/Swap using T.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 template <class T> void swap(T &a, T &b) {
-    a = a + b - (b = a);
+    a -= (b = (a += b) - b);
 }
 
 int main() {
@@ -9,11 +9,15 @@ int main() {
     float x, y;
     std::cout << "Enter two integers and two floats: ";
     std::cin >> a >> b >> x >> y;
-    std::cout << "Before swaping integers, a = " << a << " and b = " << b << std::endl;
+    std::cout << "Before swaping integers, a = ";
+    std::cout << a << " and b = " << b << std::endl;
     swap(a, b);
-    std::cout << "After swaping integers, a = " << a << " and b = " << b << std::endl;
-    std::cout << "Before swaping floats, x = " << x << " and y = " << y << std::endl;
+    std::cout << "After swaping integers, a = ";
+    std::cout << a << " and b = " << b << std::endl;
+    std::cout << "Before swaping floats, x = ";
+    std::cout << x << " and y = " << y << std::endl;
     swap(x, y);
-    std::cout << "After swaping floats, x = " << x << " and y = " << y << std::endl;
+    std::cout << "After swaping floats, x = ";
+    std::cout << x << " and y = " << y << std::endl;
     return 0;
 }
