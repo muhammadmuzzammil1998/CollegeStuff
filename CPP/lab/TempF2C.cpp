ac03d09cd4d4eefd91933dcc37f59ed8e9228508
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-09-19 23:53:15 +0530

    Update TempF2C.cpp

--- CPP/lab/TempF2C.cpp
+++ CPP/lab/TempF2C.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#define f(x) 5 * (x - 32) / 9
-#define print std::cout <<
-#define get std::cin >>
-#define n std::endl;
+#define c(x) (x - 32) / 1.8
 
 int main() {
     float x;
-    print "Enter temperature in Fahrenheit: ";
-    get x;
-    print x << " F = " << f(x) << n
+    std::cout << "Enter temperature in Fahrenheit: ";
+    std::cin >> x;
+    std::cout << x << " F = " << c(x) << " C" << std::endl;
     return 0;
 }
