203bc903ec568c0f4765e720bd32fa994a3b9943
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-09-20 00:14:32 +0530

    Update Simple Interest.cpp

--- CPP/lab/Simple Interest.cpp
+++ CPP/lab/Simple Interest.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-    int p, r, t, i;
+    float p, r, t, i;
     std::cout << "Enter Principle, Rate and Time (in months) in sequence: ";
     std::cin >> p >> r >> t;
+	t /= 12;
     i = (p * r * t) / 100;
     std::cout << "Simple interest = " << i << std::endl;
-
-    return 0;
+	return 0;
 }
