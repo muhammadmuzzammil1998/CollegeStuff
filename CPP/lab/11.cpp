969c55952aca3fc5c6880b184c9332a1bba5cd9d
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-10-19 19:38:33 +0530

    Create 11.cpp

---
+++ CPP/lab/11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cout << "Enter a year: ";
+    std::cin >> a;
+    std::cout << a << " is " << (a % 4 == 0 ? "a leap" : "not a leap") << " year.";
+    return 0;
+}
