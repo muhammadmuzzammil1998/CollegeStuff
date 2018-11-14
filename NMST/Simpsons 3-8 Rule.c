d38f15646bc2fa264c1da38fd326547d89b74c36
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-11-14 15:07:26 +0530

    Create Simpsons 3-8 Rule.c

---
+++ NMST/Simpsons 3-8 Rule.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+
+#define ACCURACY 0.0001
+#define f(x) 1 / (1 + x * x)
+
+float simpsons38(float a, float b, int n) {
+    float sum = 0;
+    float h = fabs(b - a) / n;
+    for (int i = 1; i < n; i++) {
+        float x = a + i * h;
+        sum += (i % 3 == 0 ? 2 : 3) * f(x);
+    }
+    return (3 * h / 8) * (f(a) + sum + f(b));
+}
+
+int main() {
+    float a, b, i0;
+    printf("Finding integral using Simpson's 3/8 Rule");
+    printf("\n\nf(x) = 1 / (1 + x * x)\n\n");
+    printf("Enter the limits: ");
+    scanf("%f %f", &a, &b);
+    float i1 = simpsons38(a, b, 2);
+    int i = 2;
+    do {
+        i0 = i1;
+        i++;
+        i1 = simpsons38(a, b, i);
+    } while (fabs(i1 - i0) >= ACCURACY);
+    printf("The integral is %f with %d intervals.\n", i1, i);
+    return 0;
+}
