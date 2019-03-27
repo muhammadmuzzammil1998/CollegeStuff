1b427688ab0bd04880ce7577f5ce705acc0ea9eb
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2019-03-27 20:17:39 +0530

    Create five.java

---
+++ Java/five.java
@@ -0,0 +1,10 @@
+class five {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+      for (int j = 5; j >= i; j--) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
