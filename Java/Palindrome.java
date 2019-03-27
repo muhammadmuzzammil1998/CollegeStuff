09de60f78a6cc1bc9b2ed6893f2888d8c0a62f39
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2019-03-27 20:17:24 +0530

    Create Palindrome.java

---
+++ Java/Palindrome.java
@@ -0,0 +1,14 @@
+public class Palindrome {
+  public static void main(String[] args) {
+    String rev = "";
+    String org = args[0];
+    for (int i = org.length(); i > 0; i--) {
+      rev += org.charAt(i - 1);
+    }
+    if (rev.equals(org)) {
+      System.out.println("String is palindrome");
+    } else {
+      System.out.println("String is not palindrome");
+    }
+  }
+}
