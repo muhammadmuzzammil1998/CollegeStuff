6f1714c3e248501a73001d4d6984ebdada638ac2
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2019-03-27 20:17:41 +0530

    Update run.bat

--- Java/run.bat
+++ Java/run.bat
@@ -1,4 +1,22 @@
 @echo off
-javac %1.java
-java %*
-./clear
+
+if exist runners/%1.bat (
+  if not exist bin (
+    mkdir bin
+  )
+  @echo on
+  cd runners
+  %1.bat
+  exit 0
+  @echo off
+) else if exist %1.java (
+  @echo on
+  javac %1.java
+  java %*
+  @echo off
+  clear
+  exit 0
+)
+
+echo Runner %1 doesn't exists.
+exit 1
