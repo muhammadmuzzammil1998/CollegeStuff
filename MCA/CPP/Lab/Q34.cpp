27fc612fa712878252f198c5c09d02fad56ad9ae
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2021-03-07 15:20:38 +0530

    Create Q34.cpp

---
+++ MCA/CPP/Lab/Q34.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class marks {
+ protected:
+  int _marks;
+
+ public:
+  marks(int m) : _marks(m){};
+  marks() {}
+};
+
+class student : protected marks {
+ protected:
+  int _roll_no;
+
+ public:
+  student(int r) : _roll_no(r){};
+  student() {}
+};
+
+class school : private student {
+ public:
+  void getinfo() { cout << _roll_no << "  " << _marks << endl; }
+  void setinfo() {
+    cout << "Enter roll number: ";
+    cin >> _roll_no;
+    cout << "Enter marks: ";
+    cin >> _marks;
+  }
+  school() {}
+};
+
+int main() {
+  school s1, s2;
+  s1.setinfo();
+  s2.setinfo();
+  s1.getinfo();
+  s2.getinfo();
+}
