62b157f51a5bc821bb8c5c0eb649c140e4a4bcc1
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2021-07-13 14:48:57 +0530

    Create Stack_push_array.py

---
+++ MCA/Data Structures/Stack_push_array.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self, stack=[], lim=None):
+        self._stack = stack
+        self._lim = lim
+
+    def push(self, data):
+        if self._lim is not None and self._lim > len(self._stack):
+            raise ValueError("Stack overflow.")
+        self._stack.append(int(data))
+
+    def __str__(self):
+        return str(self._stack)
+
+
+
+if __name__ == '__main__':
+    stack = Stack()
+
+    print("1. Push to stack")
+    print("2. Print stack")
+
+    while True:
+        if input("Your choice: ") == "1":
+            stack.push(input(" Enter element: "))
+        else:
+            print(stack)
