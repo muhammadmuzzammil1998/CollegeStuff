1caa5d3aae780b0db4769c4222c88000b7f44353
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2021-08-04 21:18:23 +0530

    Create PrimeNum.java

---
+++ MCA/Java/PrimeNum.java
@@ -0,0 +1,20 @@
+public class PrimeNum {
+    public static boolean IsPrime(int n) {
+        boolean prime = true;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        if (IsPrime(5)) {
+            System.out.println("Number is prime");
+        } else {
+            System.out.println("Number is not prime");
+        }
+    }
+}
