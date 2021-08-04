eaadbb263441b13e74e7318e683853730307811f
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2021-08-04 21:19:10 +0530

    Create Student.java

---
+++ MCA/Java/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    public int Roll_no;
+    public String Name;
+
+    public void Set(int rollno, String name) {
+        this.Roll_no = rollno;
+        this.Name = name;
+    }
+
+    public String GetName() {
+        return this.Name;
+    }
+
+    public int GetRollNo() {
+        return this.Roll_no;
+    }
+
+    public static void main(String[] args) {
+        Student std = new Student();
+        std.Set(8, "Muzammil");
+        System.out.println("Roll No: " + std.GetRollNo());
+        System.out.println("Name: " + std.GetName());
+    }
+}
