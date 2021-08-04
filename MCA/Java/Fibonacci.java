b4aaea9322ac74869f1791f2c9186222deeb6128
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2021-08-04 21:18:04 +0530

    Create Fibonacci.java

---
+++ MCA/Java/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
